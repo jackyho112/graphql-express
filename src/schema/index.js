486ef97c4046378aace06fcad628d6dc71d4f54d
jackyho112 <jackyho112dev@gmail.com>
2017-08-16 17:10:26 -0700

    Add mutation

---
+++ src/schema/index.js
@@ -0,0 +1,22 @@
+const {makeExecutableSchema} = require('graphql-tools');
+const resolvers = require('./resolvers');
+
+// Define your types here.
+const typeDefs = `
+  type Link {
+    id: ID!
+    url: String!
+    description: String!
+  }
+  
+  type Mutation {
+    createLink(url: String!, description: String!): Link
+  }
+
+  type Query {
+    allLinks: [Link!]!
+  }
+`;
+
+// Generate the schema object from your types definition.
+module.exports = makeExecutableSchema({typeDefs, resolvers});
