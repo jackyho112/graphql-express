3e4772f2c5aa4fda141eedbcc9e1fffc6716f967
jackyho112 <jackyho112dev@gmail.com>
2017-08-16 17:43:47 -0700

    Connec to mongo

--- src/index.js
+++ src/index.js
@@ -8,15 +8,25 @@ const bodyParser = require('body-parser');
 const {graphqlExpress, graphiqlExpress} = require('apollo-server-express');
 
 const schema = require('./schema');
+const connectMongo = require('./mongo-connector');
 
-var app = express();
-app.use('/graphql', bodyParser.json(), graphqlExpress({schema}));
+const start = async () => {
+  const mongo = await connectMongo();
+  var app = express();
 
-app.use('/graphiql', graphiqlExpress({
-  endpointURL: '/graphql',
-}));
+  app.use('/graphql', bodyParser.json(), graphqlExpress({
+    context: {mongo},
+    schema
+  }));
 
-const PORT = 3000
-app.listen(PORT, () => {
-  console.log(`Hackernews GraphQL server running on port ${PORT}.`)
-});
+  app.use('/graphiql', graphiqlExpress({
+    endpointURL: '/graphql',
+  }));
+
+  const PORT = 3000;
+  app.listen(PORT, () => {
+    console.log(`Hackernews GraphQL server running on port ${PORT}.`)
+  });
+};
+
+start();
